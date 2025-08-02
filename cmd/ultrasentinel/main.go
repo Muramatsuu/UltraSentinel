0dcbba16399b0a07087518a4ff6c496ad19b51cc
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 16:41:19 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/ultrasentinel/main.go
@@ -0,0 +1,23 @@
+// cmd/ultrasentinel/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"ultrasentinel/internal/ultrasentinel"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := ultrasentinel.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
